7aac5262573be0aaa60a03ee54aa5296ed7ac373
Aryan Dagur <kartik79907@gmail.com>
2026-03-04 18:47:11 +0530

    updated database.ts

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,32 +1,11 @@
-const { parse } = require("pg-connection-string");
+import path from 'path';
 
-module.exports = ({ env }) => {
-  // If Strapi Cloud provides the URL, parse and use it
-  if (env("DATABASE_URL")) {
-    const config = parse(env("DATABASE_URL"));
-    return {
-      connection: {
-        client: "postgres",
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: { rejectUnauthorized: false },
-        },
-      },
-    };
-  }
-
-  // Safe fallback for the build process
-  return {
+export default ({ env }: any) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client: "sqlite",
-      connection: {
-        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-      },
-      useNullAsDefault: true,
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-  };
-};
+    useNullAsDefault: true,
+  },
+});
